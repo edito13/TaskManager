1efcb339c59814aea4e0ef7688c4a340ae686d77
edito13 <editotchocosso13@gmail.com>
2023-07-18 15:36:42 +0100

    Creating a global style for all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const App = () => {
-  console.log("Caralhooo");
-
   return (
     <div>
       <h1>Hello World</h1>
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        background: #19191F;
+    }
+`;
+
+export default GlobalStyles;
